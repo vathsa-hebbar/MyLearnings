f19c7f024898e87be2aeb3dc7420303efeed6879
K Shrivathsa <68740255+vathsa-hebbar@users.noreply.github.com>
2022-08-29 16:25:06 +0530

    Create PythagoreanTripletInAnArray.cs

---
+++ C#/PythagoreanTripletInAnArray.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class PythagoreanTripletInAnArray
+{
+ 
+    // Returns true if there is a triplet
+    // with following property A[i]*A[i]
+    // = A[j]*A[j]+ A[k]*[k] Note that
+    // this function modifies given array
+    static bool isTriplet(int[] arr, int n)
+    {
+ 
+        // Square array elements
+        for (int i = 0; i < n; i++)
+            arr[i] = arr[i] * arr[i];
+ 
+        // Sort array elements
+        Array.Sort(arr);
+ 
+        // Now fix one element one by one
+        // and find the other two elements
+        for (int i = n - 1; i >= 2; i--) {
+            // To find the other two elements,
+            // start two index variables from
+            // two corners of the array and
+            // move them toward each other
+            // index of the first element
+            // in arr[0..i-1]
+            int l = 0;
+ 
+            // index of the last element
+            // in arr[0..i - 1]
+            int r = i - 1;
+            while (l < r) {
+ 
+                // A triplet found
+                if (arr[l] + arr[r] == arr[i])
+                    return true;
+ 
+                // Else either move 'l' or 'r'
+                if (arr[l] + arr[r] < arr[i])
+                    l++;
+                else
+                    r--;
+            }
+        }
+ 
+        // If we reach here, then
+        // no triplet found
+        return false;
+    }
+ 
+    // Driver Code
+    public static void Main()
+    {
+        int[] arr = { 3, 1, 4, 6, 5 };
+        int arr_size = arr.Length;
+        if (isTriplet(arr, arr_size) == true)
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("No");
+    }
+}
