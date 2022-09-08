ad391b7f37315da7fc6fe85cee7eebd4612c3bd2
K Shrivathsa <68740255+vathsa-hebbar@users.noreply.github.com>
2022-09-08 11:55:44 +0530

    Create Stock_buy_sell.cs

---
+++ C#/Stock_buy_sell.cs
@@ -0,0 +1,58 @@
+// C# implementation of the approach
+using System;
+
+class GFG
+{
+
+// Function to return the maximum profit
+// that can be made after buying and
+// selling the given stocks
+static int maxProfit(int []price, int start, int end)
+{
+
+	// If the stocks can't be bought
+	if (end <= start)
+		return 0;
+
+	// Initialise the profit
+	int profit = 0;
+
+	// The day at which the stock
+	// must be bought
+	for (int i = start; i < end; i++)
+	{
+
+		// The day at which the
+		// stock must be sold
+		for (int j = i + 1; j <= end; j++)
+		{
+
+			// If buying the stock at ith day and
+			// selling it at jth day is profitable
+			if (price[j] > price[i])
+			{
+
+				// Update the current profit
+				int curr_profit = price[j] - price[i]
+								+ maxProfit(price, start, i - 1)
+								+ maxProfit(price, j + 1, end);
+
+				// Update the maximum profit so far
+				profit = Math.Max(profit, curr_profit);
+			}
+		}
+	}
+	return profit;
+}
+
+// Driver code
+public static void Main(String[] args)
+{
+	int []price = { 30, 50 ,40, 90, 100 };
+	int n = price.Length;
+
+	Console.Write(maxProfit(price, 0, n - 1));
+}
+}
+
+// This code is contributed by PrinciRaj1992
